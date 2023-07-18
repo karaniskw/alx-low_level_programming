67bcea2b25f285cb5e97f1db4f6a3256a5f8a50a
karaniskw <sammiekaraniw@gmail.com>
2023-07-18 07:28:01 -0700

    0x02. C - Functions, nested loops task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     printf("_putchar\n");
     return 0;
 }
