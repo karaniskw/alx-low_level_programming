b3637142fe6f48241e0549a0f7ab841bea1ad74f
karaniskw <sammiekaraniw@gmail.com>
2023-08-04 15:47:15 -0700

    0x09. C - Static libraries task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower -helps checks for lowercase character
+ * @c:is the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr -is the Entry point
+ * @haystack:is the input
+ * @needle:is the input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
+
