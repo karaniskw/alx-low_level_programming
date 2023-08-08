bdc1c3cca5483a25df72fe9d75555042a69d448b
karaniskw <sammiekaraniw@gmail.com>
2023-08-08 13:33:17 -0700

    0x0B. C - malloc, free task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * count_word -this is the helper function to count the number of words in a string
+ * count_word - the helper function to count the number of words in a string
  * @s:this  string helps to evaluate
  *
  * Return: number of words
