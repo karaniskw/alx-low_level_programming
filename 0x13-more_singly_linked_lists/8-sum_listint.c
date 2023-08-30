99cf468ae54b38eec6aac647cbacd2c5ce6aae9a
karaniskw <sammiekaraniw@gmail.com>
2023-08-29 19:18:27 -0700

    0x13. C - More singly linked lists task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - helps calculate the sum of all the data in a listint_t list
+ * @head:reps first node in the linked list
+ *
+ * Return:aids return the  resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
+
